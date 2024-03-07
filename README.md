95187515f944d1836ff7a82980a6b359a77f2643
Om Pant <ompant992@gmail.com>
2024-03-07 22:34:07 +0530

    Initial Commit: setup Project using Vite and Tailwind

---
+++ README.md
@@ -0,0 +1,3 @@
+# Currency Converter React App
+
+This is a Currency Converter app built using React and Tailwind CSS.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="text-4xl bg-orange-500 p-6 text-center">
+			<h1>Currency Converter</h1>
+		</div>
+	);
+}
+
+export default App;
